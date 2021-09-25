9374afe86ae6fa53c9717654b99a6278728c6194
PavPavv <pavpavdev@gmail.com>
2021-09-25 13:34:46 +0400

    added correct path

--- src/task1-2.js
+++ src/task1-2.js
@@ -1,7 +1,8 @@
 const fs = require('fs');
+const path = require('path');
 const csv = require('csvtojson');
 
-const csvPath = './src/csv/file.csv';
+const csvPath = path.join(__dirname, './csv/file.csv');
 
 fs.readFile(csvPath, 'utf8', (error, file) => {
   if (error) {
@@ -13,7 +14,7 @@ fs.readFile(csvPath, 'utf8', (error, file) => {
     .then((jsonObj)=>{
       const str = jsonObj.map(item => JSON.stringify(item)).join('\n');
 
-      fs.writeFile('./src/file.txt', str, 'utf8', (error) => {
+      fs.writeFile(path.join(__dirname, './file.txt'), str, 'utf8', (error) => {
         console.log('file has been written');
       });
     })
